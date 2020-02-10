feaf9e4c5fb1b86e48d0f3ad458acf214753defd
Derrick Reimer <derrickreimer@gmail.com>
2020-02-10 15:44:13 -0600

    Add purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {}
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production'
+      ? {
+          '@fullhuman/postcss-purgecss': {
+            content: ['./components/**/*.js', './pages/**/*.js'],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+          }
+        }
+      : {})
   }
 };
